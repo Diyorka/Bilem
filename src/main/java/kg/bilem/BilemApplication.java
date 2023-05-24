ea1c58eb7624783237227126b522301ac2ed3eb2
Diyor Umurzakov <diyorp2@mail.ru>
2023-05-24 12:13:47 +0600

    Commit 1

---
+++ src/main/java/kg/bilem/BilemApplication.java
@@ -0,0 +1,13 @@
+package kg.bilem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kg/bilem/BilemApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.bilem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
