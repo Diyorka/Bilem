59b0e3516bd97eb7024bc76a4ff8db53259c0053
Diyor Umurzakov <teamtwo.summer@gmail.com>
2023-07-04 17:28:22 +0600

    added CourseController

---
+++ src/main/java/kg/bilem/controller/CourseController.java
@@ -0,0 +1,18 @@
+package kg.bilem.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/course")
+@AllArgsConstructor
+@CrossOrigin(origins = "*")
+@Tag(
+        name = "Контроллер для работы с курсами",
+        description = "В этом контроллере есть возможность получения, добавления, изменения и удаления курсов"
+)
+public class CourseController {
+}
